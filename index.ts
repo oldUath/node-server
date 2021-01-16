cd9a6ed8f4f7df8a534c3dc2db09cc17f5d90506
oldUath <haiyangdong666@gmail.com>
2021-01-16 17:14:22 +0800

    处理非GET请求

--- index.ts
+++ index.ts
@@ -9,6 +9,14 @@ const publicDir = p.resolve(__dirname,'public');
 server.on('request',(request,response)=>{
     const {method,url:path,headers} = request;
     const {pathname,search} = url.parse(path);
+
+    if(method !== 'GET'){
+        response.statusCode=405;
+        response.setHeader('Content-type','text/html;charset=utf-8')
+        response.end('这是静态服务器，不能接收post等动态请求');
+       return;
+    }
+
     let filename = pathname.substr(1)
     if(filename === ''){
         filename = 'index.html'
