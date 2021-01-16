2b757a8fbd20431518961d115e86580a9cba5868
oldUath <haiyangdong666@gmail.com>
2021-01-16 15:50:58 +0800

    返回404页面，并引入404图片

--- index.ts
+++ index.ts
@@ -9,13 +9,20 @@ const publicDir = p.resolve(__dirname,'public');
 server.on('request',(request,response)=>{
     const {method,url:path,headers} = request;
     const {pathname,search} = url.parse(path);
-    const filename = pathname.substr(1)
+    let filename = pathname.substr(1)
+    if(filename === ''){
+        filename = 'index.html'
+    }
     fs.readFile(p.resolve(publicDir,filename),(error,data)=>{
         if(error) {
-            response.statusCode=404;
-            response.end();
+            if(error.errno === -4058){
+                response.statusCode=404;
+                fs.readFile(p.resolve(publicDir,'404.html'),(error,data)=>{
+                    response.end(data);
+                })
+            }
         }else{
-            response.end(data.toString())
+            response.end(data)
         }
     });
 });
