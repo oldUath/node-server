906ce42cd11ca5fe8ecf3179421053a4058ec536
oldUath <haiyangdong666@gmail.com>
2021-01-16 14:58:35 +0800

    根据路径自动匹配文件

--- index.ts
+++ index.ts
@@ -8,33 +8,15 @@ const server = http.createServer();
 const publicDir = p.resolve(__dirname,'public');
 server.on('request',(request,response)=>{
     const {method,url:path,headers} = request;
-    const obj = url.parse(path);
     const {pathname,search} = url.parse(path);
-    console.log(pathname);
-    switch (pathname) {
-        case '/index.html':
-            fs.readFile(p.resolve(publicDir,'index.html'),(error,data)=>{
-                if(error)throw error
-                response.end(data.toString())
-            });
-            break;
-        case '/style.css':
-            response.setHeader('Content-type','text/css;charset=utf-8')
-            fs.readFile(p.resolve(publicDir,'style.css'),(error,data)=>{
-                if(error)throw error
-                response.end(data.toString())
-            });
-            break;
-        case '/index.js':
-            response.setHeader('Content-type','text/js;charset=utf-8')
-            fs.readFile(p.resolve(publicDir,'index.js'),(error,data)=>{
-                if(error)throw error
-                response.end(data.toString())
-            });
-            break;
-        default:
+    const filename = pathname.substr(1)
+    fs.readFile(p.resolve(publicDir,filename),(error,data)=>{
+        if(error) {
             response.statusCode=404;
             response.end();
-    }
+        }else{
+            response.end(data.toString())
+        }
+    });
 });
 server.listen(8888)
